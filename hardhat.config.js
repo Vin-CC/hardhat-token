ba1eb9199ec7fa57f8760725f7647f1d6f859fe2
Vin-CC <vincent.cance@gmail.com>
2021-12-10 18:58:41 +0100

    init project

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+task("accounts", "Prints accounts", async (_, { web3 }) => {
+  const accounts = await web3.eth.getAccounts();
+
+  for (const account of accounts) {
+    console.log(account);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+};
